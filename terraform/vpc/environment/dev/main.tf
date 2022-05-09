43580d3451b77c0e69c5a3e0c899b3e0283cbe2a
Sumitpalucc <83274234+Sumitpalucc@users.noreply.github.com>
2022-05-09 14:31:29 +0530

    Update main.tf

--- terraform/vpc/environment/dev/main.tf
+++ terraform/vpc/environment/dev/main.tf
@@ -10,3 +10,12 @@ resource "aws_vpc" "main" {
     Name = "Testremotestate"
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = "myrestatebucket"
+    key    = "path/terraform.tfstate"
+    region = "ap-south-1"
+   
+  }
+}
